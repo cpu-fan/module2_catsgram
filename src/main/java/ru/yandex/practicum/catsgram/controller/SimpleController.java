45758ef0944aa2d6863f7ce1898ea39f329700d9
Azat <a3at.valeev@gmail.com>
2022-12-24 21:54:16 +0300

    feat: подключение к БД

--- src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
@@ -1,10 +1,30 @@
 package ru.yandex.practicum.catsgram.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.catsgram.service.HackCatService;
+
+import java.util.Optional;
 
 @RestController
 public class SimpleController {
+
+    private final HackCatService hackCatService;
+
+    // внедрите бин HackCatService
+    @Autowired
+    public SimpleController(HackCatService hackCatService) {
+        this.hackCatService = hackCatService;
+    }
+
+    @GetMapping("/do-hack")
+    public Optional<String> doHack(){
+        // хакните этих котиков
+        return hackCatService.doHackNow()
+                .map(p -> "Пароль подобран! Вот он: " + p)
+                .or(() -> Optional.of("Не удалось подобрать пароль. Проверьте состояние и настройки БД."));
+    }
 
     @GetMapping("/home")
     public String homePage() {
